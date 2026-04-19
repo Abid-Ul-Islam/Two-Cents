5745149258fa3bd12c3863ac71195f2455f1713f
abid <ulislamabid3@gmail.com>
2026-04-19 16:47:55 +0600

    add the password field in the user entity

--- TwoCents WebApi/Entities/User.cs
+++ TwoCents WebApi/Entities/User.cs
@@ -5,4 +5,7 @@ public class User
     public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    
+    public string Password { get; set; }
+    public List<Blog> Blogs { get; set; } = new();
 }
