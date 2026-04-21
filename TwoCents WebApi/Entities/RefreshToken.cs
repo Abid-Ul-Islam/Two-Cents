a93845bea79882d77317eb1bb062ccc41284bc1e
Abid-Ul-Islam <ulislamabid3@gmail.com>
2026-04-21 13:44:48 +0600

    add refresh token entity

---
+++ TwoCents WebApi/Entities/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace TwoCents_WebApi.Entities;
+
+public class RefreshToken
+{
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public string UserId { get; set; } = string.Empty;
+
+    public User User { get; set; } = null!;
+}
