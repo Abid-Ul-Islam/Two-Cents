efb4bcdeff00727d9e2f8392ac41c459317055fb
Abid-Ul-Islam <ulislamabid3@gmail.com>
2026-04-22 12:50:49 +0600

    add token helper

---
+++ TwoCents WebApi/Helpers/TokenHelper.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using TwoCents_WebApi.Entities;
+
+namespace TwoCents_WebApi.Helpers;
+
+public static class TokenHelper
+{
+    public static string GenerateRefreshToken ()
+    {
+        byte[] randomBytes = new byte[64];
+        RandomNumberGenerator.Fill(randomBytes);
+        string refreshToken = Convert.ToBase64String(randomBytes)
+            .Replace("+", "-")
+            .Replace("/", "_")
+            .TrimEnd('=');
+
+        return refreshToken;
+    }
+
+    public static string GenerateAccessToken (User user)
+    {
+        SymmetricSecurityKey key = new(
+            Encoding.UTF8.GetBytes("chaitey-paro-tmi-ak-mutho-jochona.ak-mutho-golap-r-oi-nil-akash")
+        );
+
+        SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+        Claim[] claims =
+        [
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+        ];
+
+        JwtSecurityToken token = new(
+            issuer: "TwoCents_WebApi",
+            audience: "TwoCents_FrontEnd",
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(10),
+            signingCredentials: credentials
+        );
+
+        string accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return accessToken;
+    }
+
+    public static ClaimsPrincipal? VerifyAccessToken (string token)
+    {
+        JwtSecurityTokenHandler tokenHandler = new();
+
+        byte[] key = Encoding.UTF8.GetBytes(
+            "chaitey-paro-tmi-ak-mutho-jochona.ak-mutho-golap-r-oi-nil-akash"
+        );
+
+        try
+        {
+            TokenValidationParameters validationParameters = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = "TwoCents_WebApi",
+                ValidAudience = "TwoCents_FrontEnd",
+
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                ClockSkew = TimeSpan.Zero // no extra 5 min tolerance
+            };
+
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(
+                token,
+                validationParameters,
+                out SecurityToken validatedToken
+            );
+
+            return principal;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
