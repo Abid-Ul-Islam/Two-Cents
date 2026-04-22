d1f95b8e3da608864b8214a8ab13758688345283
Abid-Ul-Islam <ulislamabid3@gmail.com>
2026-04-22 15:22:38 +0600

    wip

---
+++ TwoCents WebApi/Middlewares/AuthMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using TwoCents_WebApi.Helpers;
+
+namespace TwoCents_WebApi.Middlewares;
+
+public class AuthMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public AuthMiddleware (RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync (HttpContext context)
+    {
+        string accessToken = context.Request.Cookies["AccessToken"];
+
+        ClaimsPrincipal? principal = TokenHelper.VerifyAccessToken(accessToken);
+
+        if (principal == null)
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("Ivalid/Missing token");
+            return;
+        }
+
+        await _next(context);
+
+    }
+}
--- TwoCents WebApi/Program.cs
+++ TwoCents WebApi/Program.cs
@@ -8,11 +8,31 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "TwoCents_WebApi",
+            ValidAudience = "TwoCents_FrontEnd",
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
 WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseRouting();
+
+
+
+app.UseAuthentication();
 
 app.MapControllers();
 
