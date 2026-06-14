681caff8c81279bba7bda51feb16c7cb4e3f9282
Abid-Ul-Islam <ulislamabid3@gmail.com>
2026-06-14 17:56:10 +0600

    add test controller

---
+++ TwoCents WebApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoCents_WebApi.Models;
+
+namespace TwoCents_WebApi.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class TestController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> Check (RegisterRequest registerRequest)
+    {
+        return Ok(new
+        {
+            message = "This is a test endpoint",
+        });
+    }
+}
