37d104454c97fe4185da868d9e14f8551c366c41
abid <ulislamabid3@gmail.com>
2026-06-15 14:08:05 +0600

    wip

--- TwoCents WebApi/Program.cs
+++ TwoCents WebApi/Program.cs
@@ -60,7 +60,6 @@ builder.Services
 
 WebApplication app = builder.Build();
 
-//app.UseCors("AllowReactApp");
 app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
@@ -68,6 +67,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-
-app.Run($"http://0.0.0.0:{port}");
+app.Run();
